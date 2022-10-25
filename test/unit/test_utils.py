bbe7f482cf4ebbbd6b099957c5d796466ac64d3c
Ben Rich <tiggzzss@gmail.com>
2022-10-25 16:58:52 +0000

    updated unit test wishlist

--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -6,3 +6,25 @@ import os
 import sys
 
 # TODO
+
+class TestUtils(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # TODO
+        
+    @classmethod
+    def setUp(cls):
+        # TODO
+        
+    @classmethod
+    def tearDown(cls):
+        # TODO
+        
+    def test_get_random_matrix(self):
+        # TODO
+        
+    def test_get_file_dimensions(self):
+        # TODO
+        
+    def test_write_matrix_to_file(self):
+        # TODO
