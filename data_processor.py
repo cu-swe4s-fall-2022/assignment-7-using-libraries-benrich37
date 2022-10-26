ac88aa1835b4091b945e4164a53078516d634390
Ben Rich <tiggzzss@gmail.com>
2022-10-26 19:59:06 +0000

    gave get_Random_matrix error handling

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,16 @@
 import numpy as np
+import sys
 
 def get_random_matrix(num_rows, num_columns):
-	return np.random.rand(num_rows, num_columns)
+    try: 
+        matrix = np.random.rand(num_rows, num_columns)
+    except TypeError:
+        raise TypeError('Arguments must be ints')
+    except ValueError:
+        raise ValueError('Arguments must be ints >= 1')
+    except Exception as e:
+        raise e('unexpected error')
+    return matrix
 
 def get_file_dimensions(file_name):
 	return (0,0)
