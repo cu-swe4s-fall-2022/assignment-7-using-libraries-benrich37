7329831577a4f465b0cfd176b7fa6b6b0c29b6a8
Ben Rich <tiggzzss@gmail.com>
2022-10-26 19:30:30 +0000

    prelim random matrix testing

--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -5,12 +5,18 @@ import numpy as np
 import os
 import sys
 
-# TODO
+mainpath = os.path.join(os.path.dirname(__file__), '../../')
+sys.path.append(mainpath)
+import data_processor
+
+def bet_0_1(num):
+    ret_bool = num >= 0. and num <= 1.
+    return ret_bool
 
 class TestUtils(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # TODO
+        ub_matrix_dim = 10
         
     @classmethod
     def setUp(cls):
@@ -21,7 +27,20 @@ class TestUtils(unittest.TestCase):
         # TODO
         
     def test_get_random_matrix(self):
-        # TODO
+        expected_shape = tuple([
+            random.randint(0, cls.ub_matrix_dim),
+            random.randint(0, cls.ub_matrix_dim)
+        ])
+        output_matrix = data_processor.get_random_matrix(
+            expected_shape[0],
+            expected_shape[1]
+        )
+        self.assertEqual(expected_shape,
+                        np.shape(output_matrix)
+                        )
+        for i in range(expected_shape[0]):
+            for j in range(expected_shape[1]):
+                self.assertTrue(bet_0_1(output_matrix[i][j]))
         
     def test_get_file_dimensions(self):
         # TODO
