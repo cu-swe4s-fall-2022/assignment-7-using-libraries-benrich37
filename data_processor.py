3621e58e18ee176de5a92b64aa5e52bee4114ca4
Ben Rich <tiggzzss@gmail.com>
2022-10-27 01:02:15 +0000

    idk

--- data_processor.py
+++ data_processor.py
@@ -5,9 +5,9 @@ import os
 
 
 def get_random_matrix(num_rows, num_columns):
-    
+
     """ Returns an NxM matrix of random floats within [0, 1]
-    
+
     Perameters:
     ----------
            num_rows: Number of rows (N) in return matrix
@@ -17,9 +17,9 @@ def get_random_matrix(num_rows, num_columns):
     Returns:
     --------
              matrix: NxM np.ndarray containing random floats
-                     between 0 and 1
+                       between 0 and 1
     """
-    
+
     try:
         matrix = np.random.rand(num_rows, num_columns)
     except TypeError:
@@ -32,6 +32,20 @@ def get_random_matrix(num_rows, num_columns):
 
 
 def get_file_dimensions(file_name):
+
+    """ Returns the dimensions of provided csv file
+
+    Perameters:
+    ----------
+          file_name: String of file name to examine
+                     ASSUMPTIONS: Must be a string
+                                  File must exist in working directory
+                                  File must be interpretable as a csv
+    Returns:
+    --------
+                dim: length 2 tuple with dimensions of provided file
+    """
+
     try:
         data = pd.read_csv(file_name)
     except TypeError:
@@ -40,10 +54,27 @@ def get_file_dimensions(file_name):
         raise FileNotFoundError('File ' + file_name +
                                 ' not found in working directory ' +
                                 str(os.path.dirname(__file__)))
-    return np.shape(data)
+    dim = np.shape(data)
+    return dim
 
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
+
+    """ Writes a random matrix to a csv file
+
+    Perameters:
+    ----------
+             num_rows: Passed as num_rows to get_random_matrix function
+          num_columns: Passed as num_columns to get_random_matrix function
+            file_name: String to save matrix as
+                       ASSUMPTIONS: Must be a string
+                                    Must not contain a filtype suffix
+    Returns:
+    --------
+    None
+
+    """
+
     # Expecting get_random_matrix to handle the bad argument stuff
     matrix = get_random_matrix(num_rows, num_columns)
     matrix = pd.DataFrame(matrix)
